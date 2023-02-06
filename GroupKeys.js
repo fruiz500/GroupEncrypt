d63124186f4c79a50fc19ad080d095e4741fb431
Francisco Ruiz <fruiz500@gmail.com>
2023-02-06 17:39:18 -0600

    Initial commit
    Pretty functional as it is

---
+++ GroupKeys.js
@@ -0,0 +1,21 @@
+//this object contains user names and their public keys; make sure there are commas between entries; public keys within quotes
+const GroupKeys = {
+
+Alice:
+"jxibnf9gpdpttf1acu3gamob4kt9ys8swd8ug2xaiLz2u7uqhq"
+,
+
+Bob:
+"oretLgajwccwb63iLdg0rneviL2tv507tr4h3rjwsom72k6rL6"
+,
+
+Carol:
+"tjbo7otm9pyj3xuqewLygfcx985ssbsa26wtr1dn79xb40ftsy"
+,
+
+$Girls:
+"Alice, Carol"
+
+};
+
+const headTag = new Uint8Array([27,27,27,27,27,27,27])                  //no need to change this, but it can be done
